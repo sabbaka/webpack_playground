fbeffbb77bb3c20fa82f91fd7f242c6c272f27f9
Kyrylo Gliebov <kgliebov@redhat.com>
2018-01-05 11:21:12 +0100

    Add url loader Fix optional dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   }
